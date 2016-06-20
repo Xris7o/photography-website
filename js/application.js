30a30a1aa608d23fe9852f1c5d69ec10e212c855
Vasil <vasil97@yahoo.com>
2016-06-20 19:52:34 +0000

    Function fix

--- js/application.js
+++ js/application.js
@@ -24,7 +24,7 @@ for (var i = 0; i < elems.length; i++)
  var total = 5;
 
  function slide (x) {
-    var image = document.getElementById('img');
+    var image = document.getElementById('current-slide-image');
     
     imageCount = imageCount + x;
     
@@ -36,26 +36,8 @@ for (var i = 0; i < elems.length; i++)
       imageCount = total;
     }
 
-    image.src = "js/img" + imageCount + ".JPG";
+    image.src = "img/img" + imageCount + ".JPG";
  }
-
-/* window.setInterval(function slideA(x) {
-   var image = document.getElementById('img');
-    
-    imageCount = imageCount + 1;
-    
-    if (imageCount > total) {
-      imageCount = 1;
-    }
-
-    if (imageCount < 1) {
-      imageCount = total;
-    }
-
-    image.src = "js/img" + imageCount + ".JPG";
- }, 4000);*/
- 
- /////////////////////////////////////////////////////////////////////
 
 function Timer(fn, t) {
     var timerObj = setInterval(fn, t);
@@ -87,7 +69,7 @@ function Timer(fn, t) {
 ///////////////////////////////////////////////////////////
 
 var timer = new Timer(function() {
-   var image = document.getElementById('img');
+   var image = document.getElementById('current-slide-image');
     
     imageCount = imageCount + 1;
     
@@ -99,77 +81,25 @@ var timer = new Timer(function() {
       imageCount = total;
     }
 
-    image.src = "js/img" + imageCount + ".JPG";
+    image.src = "img/img" + imageCount + ".JPG";
 }, 4000);
 
 
-// reset
-timer.reset(4000);
 
-// stop the timer
-timer.stop();
+function sliderButtonHandler (event) {
+  var element = event.target;
+  if (element.id === "previous") {
+    slide(-1);
 
-// start the timer
-timer.start();
-
-
-
-document.getElementById("reset").addEventListener("click", function() {
-  timer.reset(4000);
-    
-});
-document.getElementById("resett").addEventListener("click", function() {
-  timer.reset(4000);
-    
-});
-////////////////////////////////////////////////////////////////////////
-/* function animateLeft(obj, from, to){
-   if(from >= to){         
-       obj.style.visibility = 'hidden';
-       return;  
-   }
-   else {
-       var box = obj;
-       box.style.marginLeft = from + "px";
-       setTimeout(function(){
-           animateLeft(obj, from + 1, to);
-       }, 25) 
-   }
+  } else {
+    slide(1);
 }
 
-function animateMe() {
-animateLeft(document.getElementById('img'), 0, 400);
-}*/
+  timer.reset(4000)
+}
 
-/*  var Slider = function() { this.initialize.apply(this, arguments) }
-  Slider.prototype = {
- 
-    initialize: function(slider) {
-      this.ul = slider.children[0]
-      this.li = this.ul.children
- 
-      // make <ul> as large as all <li>’s
-      this.ul.style.width = (this.li[0].clientWidth * this.li.length) + 'px'
- 
-      this.currentIndex = 0
-    },
- 
-    goTo: function(index) {
-      // filter invalid indices
-      if (index < 0 || index > this.li.length - 1)
-        return
- 
-      // move <ul> left
-      this.ul.style.left = '-' + (100 * index) + '%'
- 
-      this.currentIndex = index
-    },
- 
-    goToPrev: function() {
-      this.goTo(this.currentIndex - 1)
-    },
- 
-    goToNext: function() {
-      this.goTo(this.currentIndex + 1)
-    }
-  }*/
+
+var sliderButtons = document.getElementsByClassName("button");
+for (i=0; i<sliderButtons.length; i++){
+  sliderButtons[i].addEventListener("click", sliderButtonHandler);
+}
